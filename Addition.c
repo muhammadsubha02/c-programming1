08791fca028ec3af2eee4459345ac4c4b316b9cf
muhammadsubha02 <s6302041620184@email.kmutnb.ac.th>
2021-01-12 16:34:55 +0700

    first commit

---
+++ Addition.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// ดึง Library Input Output
+
+
+void main(){
+//ส่วน 2 ฟังค์ชันหลักของภาษาซี
+
+//------------คำสั่ง----------
+int num1, num2, total;
+//ประกาศตัวแปร 1 2 และคำตอบ
+
+printf("value of num1 is");
+scanf("%d" ,&num);
+//----------สิ้นสุดโปรแกรม--------
+}
---
+++ HelloWorld.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+// ส่วนหัวสำหรับดึง Library(1)
+// การกด ctrl + / คือการใส่คอมเม้นต์แบบเร็ว short cut key
+
+// ส่วน function หลัก (2)
+int main()
+{
+    printf("Name: Muhammadsubha Masalaeh\n");    // ชื่อ : มูฮำหมัดศุบฮา มะสาและ
+    printf("Nick: subha \n");                    // ชื่อเล่น : ศุบฮา
+    printf("Old: 18 \n");                        // อายุ : 18
+    printf("Like: Play game \n");                // สิ่งที่ชอบ : เล่นเกมส์
+    printf("Wanna: I want to be a hacker \n");   // สิ่งที่อยากทำ : แฮ็กเกอร์
+    printf("Happynes: Smiling parents");         // สิ่งที่มีความสุข : รอยยิ้มของพ่อแม่
+     /*
+       ส่วนคำสั่งการทำงาน (3)
+       printf คือ ฟังค์ชั่นที่ไว้สำหรับแสดงผลทางหน้าจอ
+    */
+    // วิธีการจัดชุดคำสั่งให้สวย ให้ shift + alt + f   
+    // การเปิด Terminal ให้กด ctrl + j 
+}
